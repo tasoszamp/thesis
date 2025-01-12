db41be4ecdcc1600ebdaea1266f208da1c63e68d
Anastasios Zampetis <anastzampetis@gmail.com>
2025-01-12 23:26:21 +0200

    added most of the implementation and code comments

--- application/sensor_emu/sensor_sub.py
+++ application/sensor_emu/sensor_sub.py
@@ -3,24 +3,33 @@ import time
 import subprocess
 import paho.mqtt.client as mqtt_client
 
-port = 1883
-topic = "collect-data"
-hostname = socket.gethostname()
-client_id = 'subscribe-{}'.format(hostname)
-#broker = 'localhost'
-broker = 'host.docker.internal'
+port = 1883  # Default port for MQTT communication
+topic = "collect-data"  # Topic to subscribe to for data collection
+hostname = socket.gethostname()  # Get the hostname of the machine running this script. Machine (container) hostname is enforced later-on by docker compose 
+client_id = 'subscribe-{}'.format(hostname)  # Unique client ID for MQTT connection
+# broker = 'localhost'  # Uncomment this line for local broker testing
+broker = 'host.docker.internal'  # Use when running in a Docker environment
 
 def connect_mqtt():
+    """
+    Connects to the MQTT broker and sets up event handlers for connect, disconnect, and message events.
+    """
 
     def on_connect(client, userdata, flags, rc):
+        """
+        Callback triggered upon connecting to the MQTT broker.
+        """
         if rc == 0:
             print("Connected to MQTT Broker!")
-            client.subscribe(topic)
+            client.subscribe(topic)  # Subscribe to the specified topic
         else:
             print("Failed to connect, return code %d\n", rc)
     
     def on_disconnect(client, userdata, rc):
-
+        """
+        Callback triggered when the MQTT client disconnects from the broker.
+        Implements an exponential backoff strategy for reconnection attempts.
+        """
         FIRST_RECONNECT_DELAY = 1
         RECONNECT_RATE = 2
         MAX_RECONNECT_COUNT = 12
@@ -45,22 +54,26 @@ def connect_mqtt():
         print("Reconnect failed after {} attempts. Exiting...".format(reconnect_count))
 
     def on_message(client, userdata, msg):
+        """
+        Callback triggered when a message is received on the subscribed topic.
+        """
         print("Received `{}` from `{}` topic".format(msg.payload.decode(), msg.topic))
-        subprocess.run(["./sensor_data"])
+        subprocess.run(["./sensor_data"])  # Execute the sensor_data script upon message receipt
 
-
+    # Create an MQTT client instance, assign the callback functions and connect
     client = mqtt_client.Client(client_id)
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
     client.on_message = on_message
     client.connect(broker, port)
-    return client
+    return client  # Return the configured client instance
 
 def run():
+    """
+    Main function to connect the MQTT client and start its loop.
+    """
     client = connect_mqtt()
-    #subscribe(client)
     client.loop_forever()
-
 
 if __name__ == '__main__':
     run()
--- application/sensor_emu/set_startpoint.py
+++ application/sensor_emu/set_startpoint.py
@@ -1,16 +1,19 @@
 import json, random
 
 def set_startpoint():
+    """
+    Sets a STARTPOINT value based on the dataset's size and writes it to an .env file.
+    """
     with open("dataset.json") as datafile:
-        json_data = json.load(datafile)
+        json_data = json.load(datafile) # Load the dataset from a JSON file
         
-        endpoint = round(len(json_data)/10)
+        endpoint = round(len(json_data)/10) # Determine the upper limit for the STARTPOINT range
         startpoint = str(random.randint(1, endpoint))
 
         print("Setting STARTPOINT as {}".format(startpoint))
 
         with open(".env", "w") as f:
-            f.write("STARTPOINT={}".format(startpoint))
+            f.write("STARTPOINT={}".format(startpoint)) # Write the STARTPOINT to the .env file
 
 if __name__ == '__main__':
     set_startpoint()
